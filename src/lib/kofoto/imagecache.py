11f0a7e5811a42badb7404f6d36ae7f163f92b67
Joel Rosdahl <joel@rosdahl.net>
2003-04-18 19:18:29 +0000

    Simplified cache logic somewhat: Don't symlink to largest image but instead return filename of largest image. This is also better from a web browser caching view.
    Removed unnecessary "regenerate" parameter in ImageCache.get.
    
    Use image ID in image filename instead of hash.

--- src/lib/kofoto/imagecache.py
+++ src/lib/kofoto/imagecache.py
@@ -9,46 +9,44 @@ class ImageCache:
             os.mkdir(cachelocation)
 
 
-    def get(self, image, limit, regenerate=0):
+    def get(self, image, limit):
         origpath = image.getLocation()
         orientation = image.getAttribute("orientation")
-        genname = self._getCacheImageName(
-            origpath, image.getHash(), orientation, limit)
+        genname = self._getCacheImageName(image, limit)
         genpath = os.path.join(self.cachelocation, genname)
-        if not os.path.exists(genpath) or regenerate:
-            pilimg = PILImage.open(origpath)
-            width, height = pilimg.size
-            largest = max(width, height)
-            savepath = genpath
 
-            if limit < largest:
-                if width > height:
-                    size = limit, (limit * height) / width
-                else:
-                    size = (limit * width) / height, limit
-                pilimg = pilimg.resize(size)
-            elif limit > largest:
-                largestpath = os.path.join(
-                    self.cachelocation,
-                    self._getCacheImageName(
-                        origpath, image.getHash(), orientation, largest))
-                if os.path.isfile(largestpath):
-                    symlinkOrCopyFile(largestpath, genpath)
-                    return genpath
-                else:
-                    savepath = largestpath
+        if os.path.exists(genpath):
+            return genpath
 
-            if orientation:
-                if orientation == "right":
-                    pilimg = pilimg.rotate(90)
-                elif orientation == "down":
-                    pilimg = pilimg.rotate(180)
-                elif orientation == "left":
-                    pilimg = pilimg.rotate(270)
-            pilimg.save(savepath)
-            if limit > largest:
-                symlinkOrCopyFile(savepath, genpath)
-        return genpath
+        height = int(image.getAttribute("height"))
+        width = int(image.getAttribute("width"))
+        largest = max(height, width)
+        if limit > largest:
+            largestpath = os.path.join(
+                self.cachelocation,
+                self._getCacheImageName(image, largest))
+            if os.path.isfile(largestpath):
+                return largestpath
+
+        pilimg = PILImage.open(origpath)
+        savepath = genpath
+        if limit < largest:
+            if width > height:
+                size = limit, (limit * height) / width
+            else:
+                size = (limit * width) / height, limit
+            pilimg = pilimg.resize(size)
+        elif limit > largest:
+            savepath = largestpath
+
+        if orientation == "right":
+            pilimg = pilimg.rotate(90)
+        elif orientation == "down":
+            pilimg = pilimg.rotate(180)
+        elif orientation == "left":
+            pilimg = pilimg.rotate(270)
+        pilimg.save(savepath)
+        return savepath
 
 
     def cleanup(self, imagesToKeep, sizes):
@@ -57,18 +55,17 @@ class ImageCache:
             maxsize = max(int(image.getAttribute("height")),
                           int(image.getAttribute("width")))
             for size in sizes + [maxsize]:
-                keep[self._getCacheImageName(image.getLocation(),
-                                             image.getHash(),
-                                             image.getAttribute("orientation"),
-                                             size)] = 1
+                keep[self._getCacheImageName(image, size)] = 1
         for file in os.listdir(self.cachelocation):
             if not keep.has_key(file):
                 os.unlink(os.path.join(self.cachelocation, file))
 
 
-    def _getCacheImageName(self, origpath, hash, orientation, limit):
-        extension = origpath.split(".")[-1]
+    def _getCacheImageName(self, image, limit):
+        extension = image.getLocation().split(".")[-1]
+        orientation = image.getAttribute("orientation")
         if orientation not in ("up", "down", "left", "right"):
             orientation = "up"
-        genname = "%s-%s-%s.%s" % (hash, limit, orientation, extension)
+        genname = "%s-%s-%s.%s" % (image.getId(), limit,
+                                   orientation, extension)
         return genname
