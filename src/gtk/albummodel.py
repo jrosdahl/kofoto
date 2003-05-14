7e70f7fa52ec4ccca58dce7541c366451f8a2958
Ulrik Svensson <ulrik@kristnet.org>
2003-05-14 22:28:12 +0000

    Improved the album view with some ideas from Joel

--- src/gtk/albummodel.py
+++ src/gtk/albummodel.py
@@ -7,10 +7,8 @@ class AlbumModel(gtk.TreeStore):
     COLUMN_TAG       = 1
     COLUMN_TYPE      = 2
     COLUMN_OBJECT    = 3
-
-    _CYCLE_DETECTION_LEVEL = 10  # read from configuration file?
+    
     _shelf = None
-    _ignoredAlbums = FALSE
     
     def __init__(self, shelf):
         gtk.TreeStore.__init__(self,
@@ -23,29 +21,21 @@ class AlbumModel(gtk.TreeStore):
         
     def reload(self):
         self._shelf.begin()
-        self._ignoredAlbums = FALSE
-        self._buildModel(None, self._shelf.getRootAlbum(), 0, {})
+        self._buildModel(None, self._shelf.getRootAlbum(), 0, [])
         self._shelf.rollback()
-        if self._ignoredAlbums == TRUE:
-            # TODO: Show warning in pop up window instead of printing to stdout
-            print "Max album depth reached for cycle detection. All albums not showed"
 
     def _buildModel(self, parent, object, level, visited):
-        level += 1
         for child in object.getChildren():
             if child.isAlbum():
                 albumId = child.getId()
-                if level > self._CYCLE_DETECTION_LEVEL and albumId in visited:
-                    # Too depth cycle detected
-                    self._ignoredAlbums = TRUE
-                else:
-                    # Add album to model
-                    iter = self.insert_after(parent, None)
-                    self.set_value(iter, self.COLUMN_ALBUM_ID, albumId)
+                iter = self.insert_before(parent, None)
+                self.set_value(iter, self.COLUMN_ALBUM_ID, albumId)
+                self.set_value(iter, self.COLUMN_TYPE, child.getType())
+                self.set_value(iter, self.COLUMN_OBJECT, child)
+                if albumId not in visited:
                     self.set_value(iter, self.COLUMN_TAG, child.getTag())
-                    self.set_value(iter, self.COLUMN_TYPE, child.getType())
-                    self.set_value(iter, self.COLUMN_OBJECT, child)
-                    visited[albumId] = 1
-                    print "Adding: ", child.getTag()
-                    self._buildModel(iter, child, level, visited)
+                    self._buildModel(iter, child, level, visited + [albumId])
+                else:
+                    self.set_value(iter, self.COLUMN_TAG, child.getTag() + "  [...]")
+        return FALSE
 
