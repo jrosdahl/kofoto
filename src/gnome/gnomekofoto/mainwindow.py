12a6017e9bf15ac92f9c28d20209490c28365e04
Joel Rosdahl <joel@rosdahl.net>
2003-11-09 18:14:58 +0000

    Removed the attribute view.
    Removed edit and view menus.
    
    Added menu items "New", "Open" and "Save as..." to the file menu.
    
    Ignore Glade backup files in the glade directory.

--- src/gnome/gnomekofoto/mainwindow.py
+++ src/gnome/gnomekofoto/mainwindow.py
@@ -7,10 +7,9 @@ from environment import env
 class MainWindow(gtk.Window):
     def __init__(self):
         self._toggleLock = gtk.FALSE
-        
+
         env.widgets["expandViewToggleButton"].connect("toggled", self._toggleExpandView)
         env.widgets["expandViewToggleButton"].get_child().add(self.getIconImage("fullscreen-24.png"))
-        env.widgets["attributeToggleButton"].connect("toggled", self._toggleAttributesView)
         env.widgets["thumbnailsViewToggleButton"].connect("clicked", self._toggleThumbnailsView)
         env.widgets["objectViewToggleButton"].connect("clicked", self._toggleObjectView)
         env.widgets["tableViewToggleButton"].connect("clicked", self._toggleTableView)
@@ -18,11 +17,11 @@ class MainWindow(gtk.Window):
         env.widgets["quit"].connect("activate", env.controller.quit)
 
         # Gray out not yet implemented stuff...
+        env.widgets["new"].set_sensitive(gtk.FALSE)
+        env.widgets["open"].set_sensitive(gtk.FALSE)
+        env.widgets["save_as"].set_sensitive(gtk.FALSE)
         env.widgets["revert"].set_sensitive(gtk.FALSE)
-        env.widgets["copy"].set_sensitive(gtk.FALSE)
-        env.widgets["paste"].set_sensitive(gtk.FALSE)
-        env.widgets["preferences"].set_sensitive(gtk.FALSE)
-        
+
     def getIconImage(self, name):
         pixbuf = gtk.gdk.pixbuf_new_from_file(os.path.join(env.iconDir, name))
         image = gtk.Image()
@@ -35,12 +34,6 @@ class MainWindow(gtk.Window):
             env.widgets["sourceNotebook"].hide()
         else:
             env.widgets["sourceNotebook"].show()
-
-    def _toggleAttributesView(self, button):
-        if button.get_active():
-            env.widgets["attributeView"].show()
-        else:
-            env.widgets["attributeView"].hide()
 
     def _toggleThumbnailsView(self, button):
         if not self._toggleLock:
@@ -59,7 +52,7 @@ class MainWindow(gtk.Window):
             env.widgets["thumbnailsViewToggleButton"].set_active(gtk.FALSE)
             env.controller.showSingleImageView()
             self._toggleLock = gtk.FALSE
-            
+
     def _toggleTableView(self, button):
         if not self._toggleLock:
             self._toggleLock = gtk.TRUE
