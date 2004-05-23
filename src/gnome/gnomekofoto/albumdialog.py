1b6c82dbe066698e492768ea83a0dc681a01440f
Ulrik Svensson <ulrik@kristnet.org>
2004-05-23 21:20:56 +0000

    Added context menu for creating and destroying albums and editing album properties

---
+++ src/gnome/gnomekofoto/albumdialog.py
@@ -0,0 +1,39 @@
+import gtk
+import string
+import re
+from environment import env
+from gnomekofoto.taganddescriptiondialog import *
+
+class AlbumDialog(TagAndDescriptionDialog):
+    def __init__(self, title, albumId=None):
+        if albumId:
+            self._album = env.shelf.getAlbum(albumId)
+            tagText = self._album.getTag()
+            descText = self._album.getAttribute(u"title")
+            if descText == None:
+                descText = u""
+        else:
+            self._album = None
+            tagText = u""
+            descText = u""
+        TagAndDescriptionDialog.__init__(self, title, tagText, descText)
+        label = self._widgets.get_widget("titleLabel")
+        label.set_label(u"Title:")
+
+    def _isTagOkay(self, tagString):
+        try:
+           # Check that the tag name is valid.
+           verifyValidAlbumTag(tagString)
+        except BadAlbumTagError:
+            return False
+        try:
+            album = env.shelf.getAlbum(tagString)
+            if album == self._album:
+                # The tag exists, but is same as before.
+                return True
+            else:
+                # The tag is taken by another album.
+                return False
+        except AlbumDoesNotExistError:
+            # The tag didn't exist.
+            return True
--- src/gnome/gnomekofoto/albums.py
+++ src/gnome/gnomekofoto/albums.py
@@ -2,30 +2,40 @@ import gtk
 import gobject
 import gtk
 from environment import env
+from albumdialog import AlbumDialog
+from menuhandler import *
 
 class Albums:
 
 ###############################################################################            
 ### Public
 
+    __createAlbumLabel = "Create child album"
+    __destroyAlbumLabel = "Destroy album"
+    __editAlbumLabel = "Album properties"
+    
     # TODO This class should probably be splited in a model and a view when/if
     #      a multiple windows feature is introduced.
     
     def __init__(self, mainWindow):
         self.__albumModel = gtk.TreeStore(gobject.TYPE_INT,      # ALBUM_ID
                                           gobject.TYPE_STRING,   # TAG
-                                          gobject.TYPE_STRING,   # TYPE
+                                          gobject.TYPE_STRING,   # TEXT
+                                          gobject.TYPE_STRING,   # TYPE                                          
                                           gobject.TYPE_BOOLEAN)  # SELECTABLE
         self.__mainWindow = mainWindow
-        albumView = env.widgets["albumView"]
-        albumView.set_model(self.__albumModel)
+        self.__albumView = env.widgets["albumView"]
+        self.__albumView.set_model(self.__albumModel)
         renderer = gtk.CellRendererText()
-        column = gtk.TreeViewColumn("Albums", renderer, text=self.__COLUMN_TAG)
+        column = gtk.TreeViewColumn("Albums", renderer, text=self.__COLUMN_TEXT)
         column.set_clickable(True)
-        albumView.append_column(column)
-        albumSelection = albumView.get_selection()
-        albumSelection.connect('changed', self._albumSelectionHandler)
+        self.__albumView.append_column(column)
+        albumSelection = self.__albumView.get_selection()
+        albumSelection.connect('changed', self._albumSelectionUpdated)
         albumSelection.set_select_function(self._isSelectable, self.__albumModel)
+        self.__contextMenu = self.__createContextMenu()
+        self._albumSelectionUpdated()
+        self.__albumView.connect("button_press_event", self._button_pressed)
         self.loadAlbumTree()
 
     def loadAlbumTree(self):
@@ -41,19 +51,82 @@ class Albums:
     def _isSelectable(self, path, model):
         return model[path][self.__COLUMN_SELECTABLE]
 
-    def _albumSelectionHandler(self, selection):
-        albumModel, iter = selection.get_selected()
+    def _albumSelectionUpdated(self, selection=None):
+        if not selection:
+            selection = self.__albumView.get_selection()
+        albumModel, iter =  self.__albumView.get_selection().get_selected()
+        destroyMenuItem = self.__menuGroup[self.__destroyAlbumLabel]
+        editMenuItem = self.__menuGroup[self.__editAlbumLabel]
         if iter:
             albumTag = albumModel.get_value(iter, self.__COLUMN_TAG)
             self.__mainWindow.loadUrl("album://" + albumTag)
+            destroyMenuItem.set_sensitive(True)
+            editMenuItem.set_sensitive(True)            
+        else:
+            destroyMenuItem.set_sensitive(False)
+            editMenuItem.set_sensitive(False)
+
+    def _createChildAlbum(self, *dummies):
+        dialog = AlbumDialog("Create album")
+        dialog.run(self._createAlbumHelper)
+
+    def _createAlbumHelper(self, tag, desc):
+        newAlbum = env.shelf.createAlbum(tag)
+        if len(desc) > 0:
+            newAlbum.setAttribute(u"title", desc)
+        albumModel, iter =  self.__albumView.get_selection().get_selected()
+        selectedAlbumTag = albumModel.get_value(iter, self.__COLUMN_TAG)
+        selectedAlbum = env.shelf.getAlbum(selectedAlbumTag)
+        children = list(selectedAlbum.getChildren())
+        children.append(newAlbum)
+        selectedAlbum.setChildren(children)
+        # TODO The whole tree should not be reloaded
+        self.loadAlbumTree()
+        # TODO update objectCollection?
         
+    def _destroyAlbum(self, *dummies):
+        # TODO add confirmation dialog?
+        albumModel, iter =  self.__albumView.get_selection().get_selected()
+        selectedAlbumTag = albumModel.get_value(iter, self.__COLUMN_TAG)
+        env.shelf.deleteAlbum(selectedAlbumTag.decode("utf-8"))
+        # TODO The whole tree should not be reloaded
+        self.loadAlbumTree()
+        # TODO update objectCollection?
+
+    def _editAlbum(self, *dummies):
+        albumModel, iter =  self.__albumView.get_selection().get_selected()
+        selectedAlbumId = albumModel.get_value(iter, self.__COLUMN_ALBUM_ID)
+        dialog = AlbumDialog("Edit album", selectedAlbumId)
+        dialog.run(self._editAlbumHelper)
+
+    def _editAlbumHelper(self, tag, desc):
+        albumModel, iter =  self.__albumView.get_selection().get_selected()
+        selectedAlbumTag = albumModel.get_value(iter, self.__COLUMN_TAG)
+        selectedAlbum = env.shelf.getAlbum(selectedAlbumTag)
+        selectedAlbum.setTag(tag)
+        if len(desc) > 0:
+            selectedAlbum.setAttribute(u"title", desc)
+        else:
+            selectedAlbum.deleteAttribute(u"title")
+        # TODO The whole tree should not be reloaded
+        self.loadAlbumTree()
+        # TODO update objectCollection?
+        
+    def _button_pressed(self, treeView, event):
+        if event.button == 3:
+            self.__contextMenu.popup(None,None,None,event.button,event.time)
+            return True
+        else:
+            return False
+            
 ###############################################################################        
 ### Private
 
     __COLUMN_ALBUM_ID   = 0
     __COLUMN_TAG        = 1
-    __COLUMN_TYPE       = 2
-    __COLUMN_SELECTABLE = 3
+    __COLUMN_TEXT       = 2
+    __COLUMN_TYPE       = 3
+    __COLUMN_SELECTABLE = 4
 
     def __loadAlbumTreeHelper(self, parentAlbum=None, album=None, visited=[]):
         if not album:
@@ -64,10 +137,26 @@ class Albums:
         self.__albumModel.set_value(iter, self.__COLUMN_TYPE, album.getType())
         self.__albumModel.set_value(iter, self.__COLUMN_TAG, album.getTag())
         self.__albumModel.set_value(iter, self.__COLUMN_SELECTABLE, True)
+        albumTitle = album.getAttribute(u"title")
+        if albumTitle == None or len(albumTitle) < 1:
+            self.__albumModel.set_value(iter, self.__COLUMN_TEXT, album.getTag())
+        else:
+            self.__albumModel.set_value(iter, self.__COLUMN_TEXT, albumTitle)
         if album.getId() not in visited:
             for child in album.getAlbumChildren():
                 self.__loadAlbumTreeHelper(iter, child, visited + [album.getId()])
         else:
             iter = self.__albumModel.insert_before(iter, None)
-            self.__albumModel.set_value(iter, self.__COLUMN_TAG, "[...]")                        
+            self.__albumModel.set_value(iter, self.__COLUMN_TEXT, "[...]")
             self.__albumModel.set_value(iter, self.__COLUMN_SELECTABLE, False)
+
+    def __createContextMenu(self):
+        self.__menuGroup = MenuGroup()
+        self.__menuGroup.addMenuItem(self.__createAlbumLabel,
+                                     self._createChildAlbum)
+        self.__menuGroup.addMenuItem(self.__destroyAlbumLabel,
+                                     self._destroyAlbum)
+        self.__menuGroup.addMenuItem(self.__editAlbumLabel,
+                                     self._editAlbum)
+        return self.__menuGroup.createGroupMenu()
+
