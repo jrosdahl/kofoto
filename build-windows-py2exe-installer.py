413ba3026a67aa74779b1a511ce72559a1f280e5
Joel Rosdahl <joel@rosdahl.net>
2005-02-11 22:43:50 +0000

    Created .iss file template and added code for generating .iss file from the template.
    Added license files for all bundled software for inclusion in the
    Windows installer package.

--- build-windows-py2exe-installer.py
+++ build-windows-py2exe-installer.py
@@ -6,6 +6,9 @@ import py2exe
 import setup
 import shutil
 import sys
+import _winreg
+import msvcrt
+from os.path import join, isdir, basename
 
 options = {
     "py2exe": {
@@ -21,20 +24,37 @@ sys.argv = [sys.argv[0], "py2exe"]
 setup.run(options=options, windows=windows)
 
 os.unlink("kofoto.py")
-shutil.rmtree(glob.glob(os.path.join("dist", "tcl"))[0])
-os.remove(glob.glob(os.path.join("dist", "tcl*.dll"))[0])
-os.remove(glob.glob(os.path.join("dist", "tk*.dll"))[0])
+shutil.rmtree(glob.glob("dist/tcl")[0])
+os.remove(glob.glob("dist/tcl*.dll")[0])
+os.remove(glob.glob("dist/tk*.dll")[0])
 
-import _winreg
-import msvcrt
 k = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, "Software\\GTK\\2.0")
 gtkdir = _winreg.QueryValueEx(k, "Path")[0]
 for dir in ["etc", "lib", "share"]:
-    destdir = os.path.join("dist", dir)
-    if not os.path.isdir(destdir):
+    destdir = join("dist", dir)
+    if not isdir(destdir):
         os.mkdir(destdir)
-    for subdir in glob.glob(os.path.join(gtkdir, dir, "*")):
-        subdestdir = os.path.join(destdir, os.path.basename(subdir))
-        print "%s --> %s" % (subdir, subdestdir)
-        if not os.path.isdir(subdestdir):
+    for subdir in glob.glob(join(gtkdir, dir, "*")):
+        subdestdir = join(destdir, basename(subdir))
+        print "copying %s --> %s" % (subdir, subdestdir)
+        if not isdir(subdestdir):
             shutil.copytree(subdir, subdestdir)
+
+shutil.copy("COPYING.txt", "dist/license.txt")
+license_file = open("dist/license.txt", "a")
+for x in ["python", "gtk", "pygtk", "pil", "pysqlite"]:
+    f = open("packaging/%s-license.txt" % x)
+    license_file.write("\n")
+    license_file.write(f.read())
+
+versionDict = {}
+execfile("src/lib/kofoto/version.py", versionDict)
+
+print "creating kofoto.iss"
+template = \
+    open("packaging/windows/kofoto.iss.template").read() \
+        .replace("%version%", versionDict["version"]) \
+        .replace("%licensefile%", join(os.getcwd(), "dist", "license.txt")) \
+        .replace("%distdir%", join(os.getcwd(), "dist"))
+issfile = open("kofoto.iss", "w")
+issfile.write(template)
